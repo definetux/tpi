aff406274c7dbf2c98c58e6efa319bf6372ef394
definetux <goncharuk.m.n@gmail.com>
2014-12-21 20:50:32 +0300

    Added CUIT and Selenium WebDriver projects

---
+++ Lab3_1/SeleniumWebDriverTests/GoogleTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumWebDriverTests
+{
+    [TestFixture]
+    public class GoogleTests
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+
+        [Test]
+        public void TestGoogle()
+        {
+            _driver.Navigate().GoToUrl("http://www.google.com");
+            IWebElement query = _driver.FindElement(By.Name("q"));
+            query.SendKeys("unit testing");
+            query.Submit();
+            _driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(3));
+
+            IWebElement link = _driver.FindElement(By.LinkText("Unit testing - Wikipedia, the free encyclopedia"));
+            link.Click();
+            _driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(3));
+
+            IWebElement search = _driver.FindElement(By.Name("search"));
+            search.SendKeys("NUnit");
+            search.Submit();
+
+            _driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(1));
+
+            IWebElement ru = _driver.FindElement(By.ClassName("interwiki-ru"));
+
+            Assert.That(_driver.Url, Is.EqualTo("http://en.wikipedia.org/wiki/NUnit"));
+            Assert.That(ru, Is.Not.Null);
+        }
+    }
+}
