812f0303caea5805811a0dd42a00d6774651e34f
definetux <goncharuk.m.n@gmail.com>
2014-12-21 20:22:23 +0300

    Add CUIT

--- Lab3_1/CodeUITests/CodedUITest1.cs
+++ Lab3_1/CodeUITests/CodedUITest1.cs
@@ -4,6 +4,7 @@ using System.Text.RegularExpressions;
 using System.Windows.Input;
 using System.Windows.Forms;
 using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITest.Common.UIMap;
 using Microsoft.VisualStudio.TestTools.UITesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VisualStudio.TestTools.UITest.Extension;
@@ -23,9 +24,66 @@ namespace CodeUITests
         }
 
         [TestMethod]
-        public void CodedUITestMethod1()
+        public void AddGoodPathTest()
         {
-            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            this.UIMap.AddGoodPath();
+            this.UIMap.AssertTruePathList();
+        }
+
+        [TestMethod]
+        public void AddBadPathTest()
+        {
+            this.UIMap.AddBadPath();
+            this.UIMap.AssertBadPathList();
+        }
+
+        [TestMethod]
+        public void MoveTruePathTest()
+        {
+            this.UIMap.AddGoodPath();
+            this.UIMap.MoveTruePath();
+            this.UIMap.AssertBadPathListAfterMove();
+        }
+
+        [TestMethod]
+        public void RemovePathFromBadPathListTest()
+        {
+            this.UIMap.AddBadPath();
+            this.UIMap.RemoveBadPath();
+            this.UIMap.AssertBadPathListAfterRemove();
+        }
+
+        [TestMethod]
+        public void RemovePathFromTruePathListTest()
+        {
+            this.UIMap.AddGoodPath();
+            this.UIMap.RemoveTruePath();
+            this.UIMap.AssertTruePathListAfterRemoving();
+        }
+
+        [TestMethod]
+        public void BackBadPathTest()
+        {
+            this.UIMap.AddBadPath();
+            this.UIMap.BackBadPath();
+            this.UIMap.AssertInputAfterBacking();
+        }
+
+        [TestMethod]
+        public void AddEmptyInputTest()
+        {
+
+            this.UIMap.AddEmptyPath();
+            this.UIMap.AssertMessageAfterAddingEmptyPath();
+        }
+
+        [TestMethod]
+        public void BackEmptyBadPathTest()
+        {
+
+            this.UIMap.BackEmptyBadPath();
+            this.UIMap.AssertMessageAfterBackingEmptyBadPath();
+
         }
 
         #region Additional test attributes
@@ -64,5 +122,20 @@ namespace CodeUITests
             }
         }
         private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
     }
 }
